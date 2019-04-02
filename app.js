8adc7a836c1f2c7766480f3ac3e64c7e1fae336f
Unknown <findryankurnia@gmail.com>
2019-04-03 00:11:36 +0700

    add server

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require ('express');
+var nunjucks  = require('nunjucks');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+// Apply nunjucks and add custom filter and function (for example). 
+app.set('view engine', 'njk');
+var env = nunjucks.configure(['views/'], { // set folders with templates
+    autoescape: true, 
+    express: app
+});
+
+env.addFilter('myFilter', function(obj, arg1, arg2) {
+    console.log('myFilter', obj, arg1, arg2);
+    // Do smth with obj
+    return obj;  
+});
+env.addGlobal('myFunc', function(obj, arg1) { 
+    console.log('myFunc', obj, arg1);
+    // Do smth with obj
+    return obj;
+});
+
+app.get('/', function(req, res){
+    res.render('index.njk', {title: 'Main page'});    
+});
+
+app.get('/products', function(req, res){
+    res.render('products.njk', {title: 'Main page'});    
+});
+
+app.get('/blog', function(req, res){
+    res.render('blog.njk', {title: 'Main page'});    
+});
+
+app.get('/blogsingle', function(req, res){
+    res.render('blog-single.njk', {title: 'Main page'});    
+});
+
+app.get('/foo', function(req, res){
+    res.locals.smthVar = 'This is Sparta!';
+    res.render('foo.njk', {title: 'Foo page'});    
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000...');
+});
